bc3ae61c551168e9d10b30bb158120c3ad49f256
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-08 20:54:27 +0800

    binary search

--- script.js
+++ script.js
@@ -97,3 +97,29 @@ btnNew.addEventListener('click', init);
 commit
 
 */
+
+/*
+ *public class Solution {
+  public int BinarySearch(int[] nums, int target) {
+    int l = 0, r = nums.Length - 1;
+
+    while (l <= r) {
+      int mid = l - (r - l) / 2;
+      if (nums[mid] > target) {
+         l = mid - 1;
+      }
+      else if (nums[mid] < target) {
+        r = mid + 1;
+      }
+      else {
+        return mid;
+      }
+    }
+
+    return -1 ;
+  }
+ }
+ *
+ *
+ *
+ * */
